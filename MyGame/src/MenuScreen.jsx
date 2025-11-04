9e6d35b4790e708bce457bb1b1fa02a9bcc5a209
Octavian Stefan <stefanoctavian12345@gmail.com>
2025-11-04 13:57:25 +0200

    V 0.01 Added files / barebone menu
    I've added the barebone files and menu

---
+++ MyGame/src/MenuScreen.jsx
@@ -0,0 +1,16 @@
+
+function MenuScreen() {
+    
+  return(
+    <>
+      <div className="menu-screen">
+        <h1>Main Menu</h1>
+        <button>Start!</button>
+        <button>Options</button>
+        <button>Stats</button>
+      </div>
+    </>
+  )
+}
+
+export default MenuScreen
